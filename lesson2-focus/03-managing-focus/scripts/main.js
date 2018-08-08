b7efe51ad69c462ddf51b388d5d62e10b713f05a
Ajala Ayodotun <dotunajala@gmail.com>
2018-08-08 11:08:26 +0100

    Managed focus

--- lesson2-focus/03-managing-focus/scripts/main.js
+++ lesson2-focus/03-managing-focus/scripts/main.js
@@ -16,9 +16,9 @@ page('/:slug', function(context) {
   // Add is-active class to new menu item and section using the URL slug
   var newMenuItem = document.querySelector('#menu [data-page='+slug+']');
   var newPage = document.querySelector('main [data-page='+slug+']');
+  var h2text = document.querySelector('.'+slug+'').focus();
   newMenuItem.classList.add('is-active');
   newPage.classList.add('is-active');
-
 });
 
 page({
